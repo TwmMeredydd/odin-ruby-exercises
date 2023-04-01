5c2607aa5d55ebe883175a6cdbdb8e425b141501
TwmMeredydd <twmmeredydd@gmail.com>
2023-04-01 22:35:19 +0100

    Finish bubble_sort method

--- bubble_sort/lib/bubble_sort.rb
+++ bubble_sort/lib/bubble_sort.rb
@@ -1,3 +1,14 @@
 def bubble_sort(arr)
-
+    unsorted = true
+    while unsorted do
+        unsorted = false
+        (0..arr.length-2).each do |i|
+            if arr[i] > arr[i+1]
+                unsorted = true
+                arr = arr.insert(i+2, arr[i])
+                arr.delete_at(i)
+            end
+        end
+    end
+    arr
 end
