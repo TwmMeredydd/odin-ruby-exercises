a1298b73d4571a7acf6a52068387aa8f72ae8b44
TwmMeredydd <twmmeredydd@gmail.com>
2023-04-01 21:17:04 +0100

    Make find_substrings look better

--- substrings/lib/substrings.rb
+++ substrings/lib/substrings.rb
@@ -1,8 +1,6 @@
 def find_substrings(string, dictionary)
     l = string.downcase
     results = Hash.new
-    for substring in dictionary do
-        results[substring] = l.scan(/#{substring}/i).length
-    end
+    dictionary.each { |s| results[s] = l.scan(/#{s}/i).length }
     results
 end
