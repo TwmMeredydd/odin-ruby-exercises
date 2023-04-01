865af72892789085f3fd40eb770e93ca6182f9db
TwmMeredydd <twmmeredydd@gmail.com>
2023-04-01 21:22:16 +0100

    Add another test

--- substrings/spec/substrings_spec.rb
+++ substrings/spec/substrings_spec.rb
@@ -5,5 +5,9 @@ describe "Substring finder" do
         it "counts 'ad' 3 times in 'abadfhdhwkadkkeoadj'" do
             expect(find_substrings('abadfhdhwkadkkeoadj', ['ad'])).to eq({"ad" => 3})
         end
+
+        it "correctly counts occurrences of 'and' and 'ed' regardless of capitals" do
+            expect(find_substrings('And he huffEd aND he puffeD AnD he blew the house down', ['and', 'ed'])).to eq({'and' => 3, 'ed' => 2})
+        end
     end
 end
