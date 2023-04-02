fdd2cd8a62b2576f24fd64836b6d4b7c5ea29968
TwmMeredydd <twmmeredydd@gmail.com>
2023-04-02 14:23:01 +0100

    Develop grid display, win check and marking

--- tic_tac_toe/grid.rb
+++ tic_tac_toe/grid.rb
@@ -1,7 +1,38 @@
-class Grid
-    attr_reader :grid_array
+# frozen_string_literal: true
 
-    def initialize()
-        @grid_array = Array.new(3) { Array.new(3) {''} }
-    end
+class Grid
+  attr_reader :grid_array
+
+  def initialize
+    @grid_array = Array.new(3) { Array.new(3) { ' ' } }
+  end
+
+  def mark(player, row, col)
+    @grid_array[row][col] = player
+  end
+
+  def check_win(player, row, col)
+    return true if @grid_array.map { |r| r[col] }.uniq == [player]
+    return true if @grid_array[row].uniq == [player]
+    return true if @grid_array.each_with_index.map { |r, i| r[i] }.uniq == [player]
+    return true if @grid_array.each_with_index.map { |r, i| r[-i] }.uniq == [player]
+
+    false
+  end
+
+  def display_grid
+    puts <<-BOARD
+
+      #{display_row(@grid_array[0])}
+      —————————
+      #{display_row(@grid_array[1])}
+      —————————
+      #{display_row(@grid_array[2])}
+
+    BOARD
+  end
+
+  def display_row(row)
+    "#{row[0]} | #{row[1]} | #{row[2]}"
+  end
 end
